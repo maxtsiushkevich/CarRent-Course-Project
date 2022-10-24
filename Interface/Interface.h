30d67022435ee98349a04332a5d1f14ce81d5776
max <max.tyushkevich@gmail.com>
2022-10-24 14:19:31 +0300

    Начат интерфейс

---
+++ Interface/Interface.h
@@ -0,0 +1,24 @@
+#ifndef CARRENT_INTERFACE_H
+#define CARRENT_INTERFACE_H
+#include <string>
+using namespace std;
+
+class Interface
+{
+protected:
+    int mode; // mode 1 - admin, mode 2 - user
+    struct userdata
+    {
+        string login;
+        string password;
+    } userdata;
+public:
+    Interface();
+    ~Interface();
+    void authentication();
+    void registration();
+};
+
+
+
+#endif
